a4d426c45df848f352e84ed97d96f7e3d51796c6
Simon <zomon1@hotmail.com>
2023-11-12 21:23:53 +0100

    new designs

--- src/App.js
+++ src/App.js
@@ -149,7 +149,7 @@ function App() {
       <div className="search-bar">{/* Div till val av avdelning och sökning av produkt */}
       
         <div className="left-search">
-          <select className="avdelning" style={{width:"40%"}} onChange={handleSelect}> {/* Dropdown meny */}
+          <select className="searchBox" style={{width:"40%"}} onChange={handleSelect}> {/* Dropdown meny */}
             {options.map(option => (
               <option value={option.value}>{option.label}</option>
               ))}
@@ -158,7 +158,7 @@ function App() {
         <div className="right-search">
           <div className="right-search">
             <input 
-                style={{width:"200px"}}
+                className="searchBox"
                 placeholder="Sök efter Batch/Läkemedelsnamn"
                 onChange={(e) => {setSearchTerm(e.target.value); setAdded("")}}
               />
@@ -168,66 +168,61 @@ function App() {
 
 
       <div className="center-table">
-        <div
-          style={{
-            width: "75%",
-            height: "65vh",
-            overflowY: "auto",
-            border: "1px solid #000",
-          }}
-        >
-          <table
-            style={{
-              width: "100%",
-            }}
-          >
-            <thead>{/* OM MAN VILL LÄGGA NÅGOT I TABLE HEAD SENARE */}</thead>
-            <tbody>
-              {groupedRows.map(({ month, rows }) => (
-                <React.Fragment key={month}>
-                  <tr
-                    style={{
-                      border: "1px solid #000",
-                      textAlign: "left",
-                      height: "40px",
-                      background: "#87CEEB",
-                    }}
-                  >
-                    <td className="bold-cell" colSpan="10">
-                      {getMonthNameFromNumber(month)}
-                    </td>
-                  </tr>
-                  <tr
-                    style={{
-                      border: "1px solid #000",
-                    }}
-                  >
-                    <th>ID</th>
-                    <th>Namn på läkemedel</th>
-                    <th>Utgångsdatum</th>
-                    <th>LOT-nummer</th>
-                    <th>Produktkod</th>
-                    <th>Serienummer</th>
-                  </tr>
-                  {rows.map((row) => (
+        <div className="table-outer-div">
+          <div className="table-div">
+            <table
+              style={{
+                width: "100%",
+              }}
+            >
+              <thead>{/* OM MAN VILL LÄGGA NÅGOT I TABLE HEAD SENARE */}</thead>
+              <tbody>
+                {groupedRows.map(({ month, rows }) => (
+                  <React.Fragment key={month}>
                     <tr
-                      key={row.id}
                       style={{
-                        border: "0.5px solid grey"
+                        border: "1px solid #000",
+                        textAlign: "left",
+                        height: "40px",
+                        background: "#87CEEB",
                       }}
                     >
-                      <td>{row.id}</td>
-                      <td>{row.name}</td>
-                      <td>{row.expiration_date}</td>
-                      <td>{row.lot_nbr}</td>
-                      <td>{row.product_code}</td>
-                      <td>{row.serial_number}</td>
+                      <td className="bold-cell" colSpan="10">
+                        {getMonthNameFromNumber(month)}
+                      </td>
                     </tr>
-                  ))}
-                </React.Fragment>
-              ))}
-            </tbody>
-          </table>
+                    <tr
+                      style={{
+                        border: "1px solid #000",
+                      }}
+                    >
+                      <th>ID</th>
+                      <th>Namn på läkemedel</th>
+                      <th>Utgångsdatum</th>
+                      <th>LOT-nummer</th>
+                      <th>Produktkod</th>
+                      <th>Serienummer</th>
+                    </tr>
+                    {rows.map((row) => (
+                      <tr
+                        key={row.id}
+                        style={{
+                          border: "0.5px solid grey"
+                        }}
+                      >
+                        <td>{row.id}</td>
+                        <td>{row.name}</td>
+                        <td>{row.expiration_date}</td>
+                        <td>{row.lot_nbr}</td>
+                        <td>{row.product_code}</td>
+                        <td>{row.serial_number}</td>
+                      </tr>
+                    ))}
+                  </React.Fragment>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
 
@@ -235,30 +230,31 @@ function App() {
         <div className="manual-box"> {/* Div till Manuell input av läkemedel */}
           <div style={{height: "40px", width: "100%"}}><p className="textAdd">Lägg Till Manuellt</p></div>
 
-          <div className="manual-input">
+          <div className="manual-input-div">
             <input
+              className="manual-input"
               placeholder="Läkemedelsnamn"
               onChange={(e) => {setManName(e.target.value); setAdded("")}}              
             />
             <input
+              className="manual-input"
               placeholder="ÅÅÅÅ/MM/DD"
               onChange={(e) => {setManDate(e.target.value); setAdded("")}}
             />
             <input
+              className="manual-input"
               placeholder="Batch nr"
               onChange={(e) => {setManLot(e.target.value); setAdded("")}}
             />
-            <select className="avdelning" onChange={manHandleSelect}> {/* Dropdown meny */}
+            <select className="manual-input" onChange={manHandleSelect}> {/* Dropdown meny */}
                 {options.map(option => (
                   <option value={option.value}>{option.label}</option>
                   ))}
               </select>
             <button
               onClick={() => addManually()}
-            >
-            Lägg Till 
-            </button>
-            <p>{added}</p>
+            >Lägg Till</button>
+            <p style={{height: "20px",}}>{added}</p>
           </div>
         </div>
       : [] }            
